25cbf5f2da1cad81c77053442cf47e38b7847a67
Paris Ramírez Saldaña <paris.140815@gmail.com>
2021-02-15 22:49:32 -0600

    Empeando la lección 3

--- Leccion002/02-01-TiposDeDatosJS.js
+++ Leccion002/02-01-TiposDeDatosJS.js
@@ -8,12 +8,14 @@ Tipos de dato
 var nombre = "Carlos";
 console.log(nombre);
 
+//Reciclando la variable nombre
 nombre = 10.5;
-console.log(typeof nombre);
+console.log(typeof nombre);//Cambia el tipo al reasignar de String a number
 
 //Tipo de dato numérico
 var numero = 1000
 console.log(numero);
+console.log(typeof nombre);
 
 //Tipo de dato Object
 var objeto = {
@@ -22,6 +24,7 @@ var objeto = {
     teléfono: "5529837842"
 }
 console.log(objeto);
+console.log(typeof objeto);
 
 //Tipo de dato boolean (true,false)
 var bandera = true;
@@ -34,6 +37,7 @@ console.log(miFuncion);
 console.log(typeof miFuncion);
 
 var simbolo = Symbol("Mi símbolo");//Para crear un valor único de una variable
+console.log(simbolo);
 console.log(typeof simbolo);
 
 //Tipo clase es una función
@@ -48,11 +52,21 @@ console.log(Persona);
 
 //Tipo undefined
 var x;
-//x = undefined; //Puede asignarse también
+//x = undefined; //Puede asignarse también con esta instrucción comentada
 console.log(x);
 console.log(typeof x);
 
-//Null no es un tipo de dato (es un object)
+//Null no es un tipo de dato (es un object) y null significa ausencia de valor
 var y = null;
 console.log(y);
 console.log(typeof y);
+
+//Tipo de dato: arreglo
+var autox = ['BMW','Audi','Volvo'];
+console.log(autox);
+console.log(typeof autox);
+
+//Tipo: Empty String
+var z = "";
+console.log(z);
+console.log(typeof z);
